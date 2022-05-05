56e2fda1cce6a0d2464f2beb9592b6d969377c6f
Grish Vasilyan <vasilyan997@gmail.com>
2022-05-05 19:07:58 +0400

    install axios

---
+++ client/src/services/Axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
+
+export default axios.create({
+    baseURL: API_DOMAIN,
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: API_DOMAIN,
+});
